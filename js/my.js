de27d62f77bbdd5d5d22c0a48ab0f0a7ceac293e
fsites <fred.sites@yahoo.com>
2015-02-16 17:10:41 -0800

    made contact section, need to figure out making a form work

--- js/my.js
+++ js/my.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
 	$('.projects').hide();
+	$('.contact-form').hide();
 
-	$('#portfolio-link').click(function(){
-		$('.about').hide();
-		$('.projects').fadeIn(800);
+	$('.portfolio-link').click(function(){
+		$('.about').hide(1000);
+		$('.contact-form').hide(1000);
+		$('.projects').fadeIn(1600);
 	});
 
-	$('#about-link').click(function(){
-		$('.projects').hide();
-		$('.about').fadeIn(800);
+	$('.about-link').click(function(){
+		$('.projects').hide(1000);
+		$('.contact-form').hide(1000);
+		$('.about').fadeIn(1600);
+	});
+
+	$('.contact-link').click(function(){
+		$('.projects').hide(1000);
+		$('.about').hide(1000);
+		$('.contact-form').fadeIn(1600);
 	});
 });
